8538a898a2292c76cce16704bcc050100855a9be
ETWilliams10 <williamsevan10@gmail.com>
2021-09-20 13:52:09 -0400

    answer values correct/incorrect

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,24 @@
 var startQuizBtn = document.querySelector('#start_quiz');
 var timeLeftCounter = document.querySelector('#time_left');
 var startQuizContainer = document.querySelector('#start_quiz_container');
+var answerBtns = document.querySelectorAll('.quiz_question .answer_button');
+
+
+function submitAnswer() {
+    console.log('answer subtmitted')
+    console.log(this);
+    console.log(this.parentNode);
+    
+
+    if (this.value == 1) {
+        this.parentNode.classList.add('valid');
+    } else {
+        this.parentNode.classList.add('invalid')
+    }
+}
 
 function startQuiz() {
+    startQuizContainer.style.display = 'none';
     var timer = 300;
     setInterval(function() {
        timeLeftCounter.textContent = timer;
@@ -11,3 +27,7 @@ function startQuiz() {
 }
 
 startQuizBtn.addEventListener("click", startQuiz);
+
+for (var answerBtn of answerBtns) {
+    answerBtn.addEventListener("click", submitAnswer);
+}
